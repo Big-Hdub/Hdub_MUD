0490dd339fd6ee8a9f0908578ec47636d8c120e4
Big-Hdub <hdub-bb@hotmail.com>
2024-12-08 18:21:20 -0800

    Refactor application structure: update app initialization, remove unused files, and streamline dependencies, removed alembic files to start fresh implementing alembic.

--- game_server/app/__init__.py
+++ game_server/app/__init__.py
@@ -1,13 +1,22 @@
 from quart import Quart
 from app.models.db import init_app, initialize_db
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def create_app():
     app = Quart(__name__)
+    app.config['GINO_DRIVER'] = os.getenv('GINO_DRIVER')
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+    # Initialize other components like Gino, Blueprints, etc.
     init_app(app)
+
+    @app.before_serving
+    async def startup():
+        await initialize_db(app)
+
     return app
 
 app = create_app()
-
-@app.before_serving
-async def startup():
-    await initialize_db(app)
--- game_server/app/models/_init_.py
+++ game_server/app/models/_init_.py
@@ -1,2 +1,8 @@
-import db
-import user
+# app/models/__init__.py
+from gino import Gino
+from sqlalchemy.ext.declarative import declarative_base
+# Import your models here to ensure they are registered with Base
+from .user import User  # Example model import
+
+db = Gino()
+Base = declarative_base()
